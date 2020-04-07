64e53c102c9c12fd6e313bdd5dc2c02176f3ea53
dylanmestyanek <mestyanekdylan@gmail.com>
2020-04-07 16:54:06 -0500

    Added form validation

--- src/containers/Checkout/ContactData.jsx
+++ src/containers/Checkout/ContactData.jsx
@@ -16,7 +16,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -25,7 +29,11 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'E-Mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -34,7 +42,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -43,7 +55,13 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Zip Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -52,7 +70,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -82,7 +104,7 @@ class ContactData extends Component {
         const newOrder = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            ...formData
+            orderData: formData
         }
 
         axios.post('/orders.json', newOrder)
@@ -98,15 +120,39 @@ class ContactData extends Component {
     handleInputChange = e => {
         const {name, value} = e.target;
 
+        const isValid = this.checkValidity(value, this.state.orderForm[name].validation)
+
         this.setState({
             orderForm: {
                 ...this.state.orderForm,
                 [name]: {
                     ...this.state.orderForm[name],
-                    value: value
+                    value: value,
+                    valid: isValid
                 }
             }
         })
+
+        // console.log(isValid)
+        console.log(this.state)
+    }
+
+    checkValidity = (value, rules) => {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
     }
 
     render() {
