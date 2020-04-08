aed59429f136d83f6e7ec3a76514a8c87edc0e2d
dylanmestyanek <mestyanekdylan@gmail.com>
2020-04-08 12:42:28 -0500

    Built out the add ingredient reducer, no functionality yet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter>
+<Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</Provider>
 , document.getElementById('root'));
 registerServiceWorker();
